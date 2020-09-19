27c552b35e7a19155aa1bbd58095b0055747dc12
md2eoseo <md2eoseo@gmail.com>
2020-09-19 13:05:56 +0900

    how about class??

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "Won",
-  gender: "female",
-  age: 21,
-};
+const sister = new Human("Won", 21, "female");
 
 const sayHi = (person: Human): string => {
   return `Hello my name is ${person.name}, im ${person.age} years old, ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(sister));
 
 export {};
