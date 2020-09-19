b6f25887c1307b9fe8b098a1ffc193c40d20b4ea
md2eoseo <md2eoseo@gmail.com>
2020-09-19 12:38:03 +0900

    optional argument with '?'

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = "Tae",
+  age = 25,
+  gender = "male";
+
+const sayHi = (name, age, gender?) => {
+  console.log(`Hello my name is ${name}, im ${age} years old, ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
