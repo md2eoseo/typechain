5843cb33f971bb2d6459ed97d3db5ddedef434bb
md2eoseo <md2eoseo@gmail.com>
2020-09-19 13:21:21 +0900

    setup for blockchain 2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
   constructor(
     index: number,
     hash: string,
@@ -29,8 +40,11 @@ const genesisBlock: Block = new Block(
 
 let blockchain: [Block] = [genesisBlock];
 
-// never work!!
-// blockchain.push("stuff");
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
 
 console.log(blockchain);
 
